e0ecd7a5f29740b66467b8ad88e0a2995e943837
Oliver Oxenham <oliver.oxenham@gmail.com>
2017-02-16 22:53:44 -0800

    nothing much

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +6,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Crafty</h2>
+        </div>
+        <div className="column-container">
+            <div className="component-list"></div>
+            <div className="canvas"></div>
+            <div className="component-tree"></div>
         </div>
       </div>
     );
