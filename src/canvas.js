520a39f12b8e88861713942a6363d93ed0383841
Oliver Oxenham <oliver.oxenham@gmail.com>
2017-02-17 21:33:47 -0800

    generate two html files

---
+++ src/canvas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Canvas } from './components';
+import './canvas.css';
+
+ReactDOM.render(
+  <Canvas />,
+  document.getElementById('canvas')
+);
---
+++ src/components/Canvas/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class Canvas extends Component {
+  render() {
+    return (
+      <h1>Canvas</h1>
+    );
+  };
+}
+
+export default Canvas;
