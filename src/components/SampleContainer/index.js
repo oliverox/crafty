dd89132b2bb33dde2b35b75f9c0885f6505353a8
Oliver Oxenham <oliver.oxenham@gmail.com>
2017-02-17 23:19:51 -0800

    ability to push a dynamic component to canvas

---
+++ src/components/SampleContainer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SampleContainer extends Component {
+  render() {
+    const { children } = this.props;
+    const getRandomColor = () => {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i=0; i<6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+    return (
+      <div style={{border: `2px solid ${getRandomColor()}`}}>{children}</div>
+    )
+  }
+}
+
+export default SampleContainer;
